63a2f6af62c15b64167ea72604f96aa2782cc130
tuvshin <tuvshin.otgo@gmail.com>
2019-02-03 11:16:47 +0300

    Updated admin to support custum user model

--- app/core/models.py
+++ app/core/models.py
@@ -31,7 +31,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
